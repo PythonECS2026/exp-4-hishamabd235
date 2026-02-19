e4c6fc9828cac4f064fba92e38a8700e07f369ca
Hisham Shariq Abdullah <65910788+hishamabd235@users.noreply.github.com>
2026-02-19 23:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ time = float(input("Enter the time period (in years): "))
 
 simple_interest = (principal * rate * time) / 100
 
-print("Simple Interest:", simple_interest)
+print("Simple Interest: ", simple_interest)
