ce24171cdb8a532ac80e23ea0bfac64e0874bb6d
Hisham Shariq Abdullah <65910788+hishamabd235@users.noreply.github.com>
2026-02-19 23:09:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 # Write your code here
 print("Simple Interest Calculator")
 
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the rate of interest: "))
+principal = float(input("Enter Principal Amount: "))
+rate = float(input("Enter Rate of Interest: "))
 time = float(input("Enter Time Period in Years: "))
 
 simple_interest = principal* rate* time/100
