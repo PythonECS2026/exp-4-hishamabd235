7979e1df91c0eb6cda82d7a02cbc23d33bcc9f70
Hisham Shariq Abdullah <65910788+hishamabd235@users.noreply.github.com>
2026-02-19 23:08:30 +0530

    Update input prompt for time period clarity

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Simple Interest Calculator")
 
 principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the rate of interest: "))
-time = float(input("Enter the time period (in years): "))
+time = float(input("Enter Time Period in Years: "))
 
 simple_interest = principal* rate* time/100
 
