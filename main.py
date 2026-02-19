d6f954305d966a404ff4e4057287feb8f46193eb
Hisham Shariq Abdullah <65910788+hishamabd235@users.noreply.github.com>
2026-02-19 23:02:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # AIM: Write a Python program to calculate the simple interest based on user input.
-# Coder:
-# Date:
+# Coder: Hisham Shariq Abdullah
+# Date: 19/02/26
 
 # Write your code here
+principal = float(input("Enter Principal Amount: "))
+rate = float(input("Enter Rate of Interest: "))
+time = float(input("Enter Time Period in Years: "))
+
+simple_interest = (principal * rate * time) / 100
+
+print("\nSimple Interest:", simple_interest)
+
